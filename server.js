16c4b0867bfea3b670c0337fbf61d90002a122a1
Girivasan01 <139438190+Girivasan01@users.noreply.github.com>
2026-07-28 13:01:37 -0700

    Add allowed origin for demo environment

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ const configuredClientUrls = [
   "http://localhost:3000",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:3000",
+  "https://demo.webaac.in",
 ].filter(Boolean);
 
 const isAllowedOrigin = (origin) => {
